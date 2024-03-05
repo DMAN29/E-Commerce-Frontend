f8255481f3ca7fca6d3ece08681851372f1cd581
DMAN <ranjan.dr29@gmail.com>
2024-03-05 17:22:22 +0530

    profile and my order"

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const railway = "https://e-commerce-backend-production-c448.up.railway.app";
 const localhost = "http://localhost:8080";
-export const API_BASE_URL = `${localhost}`;
+export const API_BASE_URL = `${railway}`;
 const jwt = localStorage.getItem("jwt");
 export const api = axios.create({
   baseURL: API_BASE_URL,
