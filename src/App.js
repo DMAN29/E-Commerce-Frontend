13be24f427b210c51a5273308865e9629ab3f2db
DMAN <ranjan.dr29@gmail.com>
2024-03-18 16:51:39 +0530

    info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 
 import CustomerRouters from "./Routers/CustomerRouters";
 import AdminRouters from "./Routers/AdminRouters";
 const App = () => {
+  useEffect(
+    () =>
+      alert(
+        "Hello Users This is an information regarding the website. Its all the api's are protected so firstly we have to login or signup our Account first \n\n One of the Created Profile Details : \n Email : ram@gmail.com \n Password : ram@123"
+      ),
+    []
+  );
   return (
     <>
       <Routes>
