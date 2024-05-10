f9968445893807392eeefe6b770179f7df4346a9
DMAN <ranjan.dr29@gmail.com>
2024-05-10 22:49:42 +0530

    back to local host

--- src/Config/apiConfig.js
+++ src/Config/apiConfig.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const awsBackend =
-  "http://shopnow-env.eba-z7cqpr2m.ap-southeast-2.elasticbeanstalk.com";
-export const API_BASE_URL = `${awsBackend}`;
+// const awsBackend =
+//   "http://shopnow-env.eba-z7cqpr2m.ap-southeast-2.elasticbeanstalk.com";
+// export const API_BASE_URL = `${awsBackend}`;
 
-// const localhost = "http://localhost:5000";
-// export const API_BASE_URL = `${localhost}`;
+const localhost = "http://localhost:5000";
+export const API_BASE_URL = `${localhost}`;
 
 const jwt = localStorage.getItem("jwt");
 export const api = axios.create({
