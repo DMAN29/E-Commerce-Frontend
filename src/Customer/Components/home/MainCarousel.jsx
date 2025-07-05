4230b4d1ba0d88faee1f758c6cecea4bc5188876
DMAN29 <ranjan.dr29@gmail.com>
2025-07-05 08:06:07 +0530

    carosal change

--- src/Customer/Components/home/MainCarousel.jsx
+++ src/Customer/Components/home/MainCarousel.jsx
@@ -4,19 +4,26 @@ import "react-alice-carousel/lib/alice-carousel.css";
 import { MainCarouselData } from "../../../Data/MainCarouselData";
 
 const MainCarousel = () => {
-  const items = MainCarouselData.map((item) => (
-    <img className="cursor-pointer" role="presentation" src={item.src} alt="" />
+  const items = MainCarouselData.map((item, index) => (
+    <img
+      key={index}
+      className="w-full h-[650px] object-cover cursor-pointer"
+      role="presentation"
+      src={item.src}
+      alt={`carousel-${index}`}
+    />
   ));
+
   return (
-    <>
-      <AliceCarousel
-        items={items}
-        infinite
-        autoPlay
-        autoPlayInterval={1000}
-        disableButtonsControls
-      />
-    </>
+    <AliceCarousel
+      items={items}
+      infinite
+      autoPlay
+      autoPlayInterval={3000}
+      disableButtonsControls
+      animationDuration={800}
+    />
   );
 };
+
 export default MainCarousel;
--- src/Data/MainCarouselData.js
+++ src/Data/MainCarouselData.js
@@ -1,14 +1,17 @@
 export const MainCarouselData = [
-    {
-        src:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/5/_/5_8.jpg"
-    },
-    {
-        src:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/_/1_6.jpg"
-    },
-    {
-        src:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/3/_/3_10.jpg"
-    },
-    {
-        src:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/4/_/4_9.jpg"
-    },
-]
+  {
+    // src:"https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/5/_/5_8.jpg"
+    src: "https://ultron.zyadashop.io/xenon/https%3A%2F%2Ffirebasestorage.googleapis.com%2Fv0%2Fb%2Fshopkarle-a28da.appspot.com%2Fo%2FSTORES%252Fmenfield%252FSTORE_BANNERS%252F3_new.webp%3Falt%3Dmedia%26token%3D4dea6893-5054-489a-93e5-2cdbb4033430/2000/1000",
+  },
+  {
+    // src: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/1/_/1_6.jpg",
+    src: "https://cdn11.bigcommerce.com/s-530r49nxb3/images/stencil/original/carousel/102/spring-summer2025_homebanner2.png?c=1",
+  },
+  {
+    // src: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/3/_/3_10.jpg",
+    src: "https://www.bakersandlarners.co.uk/media/wysiwyg/brook_taverner_EDITED.jpg",
+  },
+  //   {
+  //     src: "https://www.ethnicplus.in/media/mageplaza/bannerslider/banner/image/4/_/4_9.jpg",
+  //   },
+];
